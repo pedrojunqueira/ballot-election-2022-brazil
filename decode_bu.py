51eabeee44c3300d9bb6638d03f4c4296c5a1191
pedrojunqueira <pedrocj@gmail.com>
2022-11-06 04:21:49 +1030

    added ballot report function

--- decode_bu.py
+++ decode_bu.py
@@ -50,6 +50,8 @@ for file in BU_FILES:
                                         if vote_type == 'nominal':
                                             candidate_code = vote_count['identificacaoVotavel']['codigo']
                                             print(f"candidate: {candidate_code} got {qty} votes ")
+                                        else:
+                                            print(f"{vote_type} got {qty} votes ")
                     print()
 
 
@@ -61,27 +63,4 @@ for file in BU_FILES:
         counter +=1
         print(file)
         print(e)
-
-# ['cabecalho', 
-# 'fase', 
-# 'urna', 
-# 'identificacaoSecao', 
-# 'dataHoraEmissao', 
-# 'dadosSecaoSA', 
-# 'resultadosVotacaoPorEleicao', 
-# 'chaveAssinaturaVotosVotavel']
-
-# {'cabecalho': {'dataGeracao': '20221030T170437', 'idEleitoral': ('idPleito', 407)}, 
-# 'fase': 'oficial', 
-# 'urna': {'tipoUrna': 'secao', 'versaoVotacao': '8.26.0.0 - Onça-pintada', 'correspondenciaResultado': {'identificacao': ('identificacaoSecaoEleitoral', {'municipioZona': {'municipio': 29530, 'zona': 1}, 'local': 1, 'secao': 84}), 
-#                 'carga': {'numeroInternoUrna': 1050840, 'numeroSerieFC': b'\xee\x85X\xcc', 'dataHoraCarga': '20220913T135500', 'codigoCarga': '572868572189728515131873'}}, 'tipoArquivo': 'votacaoUE', 'numeroSerieFV': b'\xe2K\x04"'}, 
-# 'identificacaoSecao': {'municipioZona': {'municipio': 29530, 'zona': 1}, 'local': 1015, 'secao': 84}, 
-# 'dataHoraEmissao': '20221030T170351', 
-# 'dadosSecaoSA': ('dadosSecao', {'dataHoraAbertura': '20221030T080001', 'dataHoraEncerramento': '20221030T170050'}), 
-# 'resultadosVotacaoPorEleicao': [{'idEleicao': 545, 'qtdEleitoresAptos': 305, 'resultadosVotacao': 
-#     [{'tipoCargo': 'majoritario', 'qtdComparecimento': 171, 'totaisVotosCargo': 
-#         [{'codigoCargo': ('cargoConstitucional', 'presidente'), 'ordemImpressao': 1, 'votosVotaveis': 
-#                                                                                             [{'tipoVoto': 'nominal', 'quantidadeVotos': 100, 'identificacaoVotavel': {'partido': 13, 'codigo': 13}, 'assinatura': b'U\xac\xdf\xac\xa5\x00N!$\x0c\x92^P\x05\xfb\xf7&\x1d1\xad\x90s\x89b\x00\x05CR`ubl\xa0G\xa0\x80\xfd\xcf0\xbe\xf7\xe4\x8cC\xfc\x90\x1emC\xffsu\xb1&d9Cr|\x81|\xc2m\n'}, {'tipoVoto': 'nominal', 'quantidadeVotos': 62, 'identificacaoVotavel': {'partido': 22, 'codigo': 22}, 'assinatura': b'\xee\xf2D\xfb=\x00s\xf4<r\xe8=M\xb0\x8b\xc42\x9a\xabZ\xc8.\x80\xec\xdd\xc8\xb4#c\x86\xb0R\xd5\xf3\\\x97t\xca\x19\xa3\xb9\xd0+)\x19\x13\xf5T\xfeP\xa6\xb3\xed\xecAg\xb7b]\xc1\xb9D\xea\x03'}, {'tipoVoto': 'branco', 'quantidadeVotos': 1, 'assinatura': b"#\x1a\xf3\x92Y\xa1\xfa0\xfa?L}\xfb\x97\r\xdd\xb6\xc7\xae%\xdc\x14w\x81\xe0wT>\x8f\xd3\xc6\xe3~\xbc*\xcf\xdc\x01XP'*\x97t\xfc\xf4\x8cA:\xde\x02\x97;\xc0\xcaO\r\xef\xe0\xb5z\x9b\xff\t"}, {'tipoVoto': 'nulo', 'quantidadeVotos': 8, 'assinatura': b't]\x0b\xe4\x84\xa1\xf7(`\xb8\xc0K\xc2\xc5M{\xf1\xea\xf9\xd4\xc7\x18\xfe$N\xc4~^I\x9d\x7f\x9f\x83`:(\\h\x97@\xa9\x87\xf1\x88\xac\x8d\x0f?\xef5\xb5\xa0\x0b1\t\xbc\xcc\x92\x0b\x9c(0u\x07'}]}]}]}], 
-# 'chaveAssinaturaVotosVotavel': b'o\xc3yB\x05\x8a\x9dM\xd4\xfd\xe0\xb3U\x95\xd7>$3\xe0\x11R\xbd\x07\xbf\xd0\xa7\xc5l\xedl\x1a\xf3'}
-
 print(counter)
--- get_ballot_bu.py
+++ get_ballot_bu.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import concurrent.futures as cf
+from datetime import datetime
 
 import requests
 
@@ -45,6 +46,31 @@ BASE_DIR = Path(".").resolve()
 bu_path = BASE_DIR / "bu"
 
 BU_FILES = [ i.name for i in bu_path.iterdir() if i.suffix == '.bu']
+
+def calculate_state_ballot_report(state_code:str)-> list:
+    total_ballots = 0
+    with open(STATE_BALLOTS_FILE_PATH.format(uf=state_code.lower()), "r+") as fp:
+        state_data = json.load(fp)
+
+    state_code = state_data["abr"][0]["cd"]
+    councils  = state_data["abr"][0]["mu"]
+
+    for council in councils:
+        # mu_cd = council["cd"]
+        zones = council["zon"]
+        for zone in zones:
+            # zone_cd = zone["cd"]
+            sections = zone["sec"]
+            for section in sections:
+                # section_cd = section["ns"]
+                total_ballots += 1
+    
+    print("###############")
+    print(f"State: {state_code}")
+    print(f"Number of councils: {len(councils)}")
+    print(f"Total number of ballots: {total_ballots}")
+    print("###############")
+    print()
 
 def get_state_ballot_codes(state_code:str)-> list:
     ballots = []
@@ -131,15 +157,41 @@ def get_reminder_ballots(batch:list)->list:
 
 state_codes = [s for s in states.keys()]
 
-for state_code in state_codes[-1:]:
-    ballots = get_state_ballot_codes("zz")
-    batches = create_batches(20, ballots)
-    for batch in batches:
-        ballots_to_process = get_reminder_ballots(batch)
-        if len(ballots_to_process) > 0:
-            print("process")
-            # with cf.ThreadPoolExecutor() as executor:
-            #     executor.map(process_ballot, batch)
+start_time = datetime.now()
+
+
+# # # download BUs from site
+
+for state_code in state_codes[5:6]:
+    try:
+        print(f"processing state_code: {state_code}")
+        ballots = get_state_ballot_codes(state_code)
+        batches = create_batches(80, ballots)
+        batch_count = 1
+        for batch in batches:
+            ballots_to_process = get_reminder_ballots(batch)
+            # print(len(ballots_to_process))
+            if len(ballots_to_process) > 0:
+                print(f"processing batch {batch_count}/{len(batches)} ")
+                with cf.ThreadPoolExecutor() as executor:
+                    executor.map(process_ballot, batch)
+                batch_count += 1
+    except Exception as e:
+        print(e)
 
 # print(len(BU_FILES))
 
+# for state_code in state_codes:
+#     calculate_state_ballot_report(state_code)
+
+
+finish_time = datetime.now()
+
+total_time = finish_time - start_time
+
+print(f"execution took {total_time.seconds} seconds")
+
+
+
+
+    
